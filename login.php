ccfbb962e622cdaab5931301611d32ce0f00caae
666333-png <riris.hasnani@student.unmer.ac.id>
2022-07-07 10:36:40 +0700

    first commit

--- login.php
+++ login.php
@@ -9,7 +9,7 @@
 <head>
 	<meta charset="utf-8">
 	<meta http-equiv="X-UA-Compatible" content="IE=edge">
-	<title>login | SI Data Pegawai</title>
+	<title>login | Data Pegawai</title>
 	<link rel="icon" href="dist/img/logo.png">
 	<!-- Tell the browser to be responsive to screen width -->
 	<meta name="viewport" content="width=device-width, initial-scale=1">
